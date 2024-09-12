705de886d3b2c7cd4d0637c21abf450f480788c1
Achmad Syidiq <achmadsyidiq@outlook.com>
2024-09-12 13:03:13 +0700

    Exercise 2.2 - 2.3 : Courseinfo step 7 - 8

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,12 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <div>
-      {parts.map((part, id) => (
-        <Part key={id} part={part} />
-      ))}
+      {parts.map((part, id) => <Part key={id} part={part} />)}
+      <Total total={total}/>
     </div>
   );
 };
