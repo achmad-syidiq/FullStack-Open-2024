860c3d07dad15a5e4f55a024b5e57576a8dab73b
Achmad Syidiq <achmadsyidiq@outlook.com>
2024-09-12 12:48:51 +0700

    Exercise 2.1 : Courseinfo step 6

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part";
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, id) => (
+        <Part key={id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+export default Content;
