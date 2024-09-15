02c58a07a7efe407452fc04c3d81ab310c465322
Achmad Syidiq <achmadsyidiq@outlook.com>
2024-09-16 02:35:04 +0700

    Promises and Errors

--- part2-1/src/App.jsx
+++ part2-1/src/App.jsx
@@ -17,14 +17,17 @@ const App = () => {
   }, [])  
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
     noteServices
-      .update(url, changedNote)
+      .update(id, changedNote)
       .then(returnedNote => {
         setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        alert(`the note '${note.content} was already deleted from server'`)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
