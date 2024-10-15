5b8ebe1751890918925c098b62698206166c3b44
Achmad Syidiq <achmadsyidiq@outlook.com>
2024-10-15 13:15:58 +0700

    Inline styles

--- part2-1/src/App.jsx
+++ part2-1/src/App.jsx
@@ -1,82 +1,85 @@
 import React, { useEffect, useState } from "react";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
-import noteServices from './services/notes';
+import noteServices from "./services/notes";
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNotes] = useState('a new note...')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('what an error happened')
+  const [newNote, setNewNotes] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("what an error happened");
 
   useEffect(() => {
-    noteServices
-      .getAll()
-      .then(initialNotes => 
-        setNotes(initialNotes)
-      )
-  }, [])  
+    noteServices.getAll().then((initialNotes) => setNotes(initialNotes));
+  }, []);
 
   const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = {...note, important: !note.important}
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
 
     noteServices
       .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      .then((returnedNote) => {
+        setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
       })
-      .catch(error => {
-        setErrorMessage(`Note '${note.content}' was already removed from server`)
+      .catch((error) => {
+        setErrorMessage(`Note '${note.content}' was already removed from server`);
         setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
-    }
-    noteServices
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNotes('')
-      })
-  }
+    };
+    noteServices.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNotes("");
+    });
+  };
 
   const handleNoteChange = (event) => {
-    setNewNotes(event.target.value)
-  }
+    setNewNotes(event.target.value);
+  };
 
-  const notesToShow = showAll ? notes : notes.filter(note=> note.important === true)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important === true);
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note 
-            key={note.id} 
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
